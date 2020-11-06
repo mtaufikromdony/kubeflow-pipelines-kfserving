2bae7aecbbce45558d08ffbd99ab9e479935e90a
Mochamad Taufik Romdony <55184187+mtaufikromdony@users.noreply.github.com>
2020-11-06 18:34:33 +0700

    to uninstall kubeflow and delete GKE cluster

---
+++ uninstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# uninstall Kubeflow Pipelines
+export PIPELINE_VERSION=1.1.0-alpha.1 
+kubectl delete -k "github.com/kubeflow/pipelines/manifests/kustomize/env/dev?ref=$PIPELINE_VERSION"
+kubectl delete -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scoped-resources?ref=$PIPELINE_VERSION"
+
+# Delete GKE Cluster
+export CLUSTER=kubeflow-pipelines-cluster
+export ZONE=us-central1-c
+gcloud container clusters delete "$CLUSTER" --zone "$ZONE"
